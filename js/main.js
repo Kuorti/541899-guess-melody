812867099c64ffae20f0117ad0fdd02702c28b66
Григорий Куорти <griiishaaa@mail.ru>
2018-11-15 02:29:20 +0300

    code style fix

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
 const aTemplatesSelectors = [`#welcome`, `#game-artist`, `#game-genre`, `#result-success`, `#fail-time`, `#fail-tries`, `#modal-error`, `#modal-confirm`];
 const aTemplates = [];
 aTemplatesSelectors.forEach(function (sSelector) {
-    aTemplates.push(document.querySelector(sSelector));
+  aTemplates.push(document.querySelector(sSelector));
 });
 
 const showTemplate = (iNumber) => {
-    const eMainBlock = document.querySelector(`.main`);
-    eMainBlock.innerHTML = ``;
-    eMainBlock.appendChild(aTemplates[iNumber]);
+  const eMainBlock = document.querySelector(`.main`);
+  eMainBlock.innerHTML = ``;
+  eMainBlock.appendChild(aTemplates[iNumber]);
 };
 
 const addNavigationArrows = () => {
-    const sElemToPaste =
+  const sElemToPaste =
         `<div class="arrows__wrap">
             <style>
               .arrows__wrap {
@@ -30,27 +30,27 @@ const addNavigationArrows = () => {
             <button class="arrows__btn"><-</button>
             <button class="arrows__btn">-></button>
         </div>`;
-    document.querySelector(`.app`).innerHTML += (sElemToPaste);
+  document.querySelector(`.app`).innerHTML += (sElemToPaste);
 };
 
 const setKeyboardListener = () => {
-    let iPageCounter = 0;
-    document.addEventListener(`keyup`, function () {
-        if (event.keyCode === 39) {
-            iPageCounter = iPageCounter === aTemplatesSelectors.length - 1 ? aTemplatesSelectors.length - 1 : ++iPageCounter;
-        } else if (event.keyCode === 37) {
-            iPageCounter = iPageCounter === 0 ? 0 : --iPageCounter;
-        }
-        showTemplate(iPageCounter);
-    });
-    document.addEventListener(`click`, function () {
-        if (event.target === document.querySelectorAll(`.arrows__btn`)[1]) {
-            iPageCounter = iPageCounter === aTemplatesSelectors.length - 1 ? aTemplatesSelectors.length - 1 : ++iPageCounter;
-        } else if (event.target === document.querySelectorAll(`.arrows__btn`)[0]) {
-            iPageCounter = iPageCounter === 0 ? 0 : --iPageCounter;
-        }
-        showTemplate(iPageCounter);
-    });
+  let iPageCounter = 0;
+  document.addEventListener(`keyup`, function () {
+    if (event.keyCode === 39) {
+      iPageCounter = iPageCounter === aTemplatesSelectors.length - 1 ? aTemplatesSelectors.length - 1 : ++iPageCounter;
+    } else if (event.keyCode === 37) {
+      iPageCounter = iPageCounter === 0 ? 0 : --iPageCounter;
+    }
+    showTemplate(iPageCounter);
+  });
+  document.addEventListener(`click`, function () {
+    if (event.target === document.querySelectorAll(`.arrows__btn`)[1]) {
+      iPageCounter = iPageCounter === aTemplatesSelectors.length - 1 ? aTemplatesSelectors.length - 1 : ++iPageCounter;
+    } else if (event.target === document.querySelectorAll(`.arrows__btn`)[0]) {
+      iPageCounter = iPageCounter === 0 ? 0 : --iPageCounter;
+    }
+    showTemplate(iPageCounter);
+  });
 };
 
 addNavigationArrows();
