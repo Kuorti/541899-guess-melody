1dc2e12877e1c1a9b68452476063585ec65b1c0e
Григорий Куорти <griiishaaa@mail.ru>
2018-11-22 00:21:33 +0300

    in work

---
+++ js/showTempByNumber.js
@@ -0,0 +1,16 @@
+import welcomeScreen from './welcomeScreen';
+import gameArtist from './gameArtist';
+import gameGenre from './gameGenre';
+import resultSuccess from './resultSuccess';
+import failTime from './failTime';
+import failTries from './failTries';
+
+const templates = [];
+
+templates.push(welcomeScreen, gameGenre, gameArtist, resultSuccess, failTime, failTries);
+
+export const showTemplate = (number) => {
+  const mainBlock = document.querySelector(`.main`);
+  mainBlock.innerHTML = ``;
+  mainBlock.appendChild(templates[number].content.cloneNode(true));
+};
