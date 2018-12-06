c876e3045105d691d27d85466acf7cd6a2f5b3f6
Григорий Куорти <griiishaaa@mail.ru>
2018-12-06 19:46:30 +0300

    added constants in utils

--- js/data/utils.js
+++ js/data/utils.js
@@ -1,36 +1,37 @@
+const maxAnswersArrayLength = 20;
+const slowAndFastAnswersEdge = 30;
+const outOfTimeMessage = `Время вышло! Вы не успели отгадать все мелодии`;
+const outOfTriesMessage = `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
 const countGamePoints = (answersValuesTime, triesLeft) => {
   let sum = null;
   for (let i = 0; i < answersValuesTime.length; i++) {
-    if (answersValuesTime.length < 20 || triesLeft === 0) {
+    if (answersValuesTime.length < maxAnswersArrayLength || triesLeft === 0) {
       sum = -1;
-    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] < 30) {
+    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] <= slowAndFastAnswersEdge) {
       sum += answersValuesTime[i] * 2;
-    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] >= 30) {
+    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] > slowAndFastAnswersEdge) {
       sum += answersValuesTime[i];
     } else if (i % 2 === 0 && answersValuesTime[i] === 0) {
       sum -= 2;
     }
   }
-
   return sum;
 };
-
 const showResults = (otherPlayersPoints, resultStatistics) => {
   if (resultStatistics.timeLeft === 0) {
-    return `Время вышло! Вы не успели отгадать все мелодии`;
+    return outOfTimeMessage;
   } else if (resultStatistics.notesLeft === 0) {
-    return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+    return outOfTriesMessage;
   } else {
     let newPlayersPoints = otherPlayersPoints.slice();
     newPlayersPoints.push(resultStatistics.points);
     newPlayersPoints.sort(compareNum).reverse();
     const playerPointsPosition = newPlayersPoints.indexOf(resultStatistics.points) + 1;
-    const playersQuantity = newPlayersPoints.length; // 4
+    const playersQuantity = newPlayersPoints.length;
     let statisticsPosition = (playersQuantity - playerPointsPosition) / playersQuantity * 100;
     return `Вы заняли ${playerPointsPosition} место из ${playersQuantity} игроков. Это лучше, чем у ${statisticsPosition}% игроков`;
   }
 };
-
 const changeLevel = (currentLevel, lastLevel) => {
   if (currentLevel < lastLevel) {
     return currentLevel + 1;
@@ -38,7 +39,6 @@ const changeLevel = (currentLevel, lastLevel) => {
     return currentLevel;
   }
 };
-
 const showTimeLeft = (timeFromStart, timeLimit) => {
   if (timeFromStart < timeLimit) {
     return timeLimit - timeFromStart;
@@ -46,9 +46,7 @@ const showTimeLeft = (timeFromStart, timeLimit) => {
     return 0;
   }
 };
-
 const compareNum = (a, b) => {
   return a - b;
 };
-
 export default {countGamePoints, showResults, changeLevel, showTimeLeft};
