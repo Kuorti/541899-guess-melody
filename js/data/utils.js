4bdebc68316af74fcf303b0e8d8e5059af6ef886
Григорий Куорти <griiishaaa@mail.ru>
2018-12-06 19:43:31 +0300

    const values added

--- js/data/utils.js
+++ js/data/utils.js
@@ -1,11 +1,16 @@
+const maxAnswersArrayLength = 20;
+const slowAndFastAnswersEdge = 30;
+const outOfTimeMessage = `Время вышло! Вы не успели отгадать все мелодии`;
+const outOfTriesMessage = `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+
 const countGamePoints = (answersValuesTime, triesLeft) => {
   let sum = null;
   for (let i = 0; i < answersValuesTime.length; i++) {
-    if (answersValuesTime.length < 20 || triesLeft === 0) {
+    if (answersValuesTime.length < maxAnswersArrayLength || triesLeft === 0) {
       sum = -1;
-    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] < 30) {
+    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] <= slowAndFastAnswersEdge) {
       sum += answersValuesTime[i] * 2;
-    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] > 30) {
+    } else if (i % 2 === 0 && answersValuesTime[i] !== 0 && answersValuesTime[i + 1] > slowAndFastAnswersEdge) {
       sum += answersValuesTime[i];
     } else if (i % 2 === 0 && answersValuesTime[i] === 0) {
       sum -= 2;
@@ -17,15 +22,15 @@ const countGamePoints = (answersValuesTime, triesLeft) => {
 
 const showResults = (otherPlayersPoints, resultStatistics) => {
   if (resultStatistics.timeLeft === 0) {
-    return `Время вышло! Вы не успели отгадать все мелодии`;
+    return outOfTimeMessage;
   } else if (resultStatistics.notesLeft === 0) {
-    return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+    return outOfTriesMessage;
   } else {
     let newPlayersPoints = otherPlayersPoints.slice();
     newPlayersPoints.push(resultStatistics.points);
     newPlayersPoints.sort(compareNum).reverse();
     const playerPointsPosition = newPlayersPoints.indexOf(resultStatistics.points) + 1;
-    const playersQuantity = newPlayersPoints.length; // 4
+    const playersQuantity = newPlayersPoints.length;
     let statisticsPosition = (playersQuantity - playerPointsPosition) / playersQuantity * 100;
     return `Вы заняли ${playerPointsPosition} место из ${playersQuantity} игроков. Это лучше, чем у ${statisticsPosition}% игроков`;
   }
